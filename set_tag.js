4225ff1e2577b2ca76dcd2b87be50053852c4d0a
shynome <shynome@gmail.com>
2018-07-26 01:24:48 +0800

    aa

--- set_tag.js
+++ set_tag.js
@@ -1,6 +1,8 @@
 
-const version = /**@type {string}*/(process.env.npm_package_version)
+const [ version, suffix='' ] = /**@type {string}*/(process.env.npm_package_version).split('-')
 
-const versions = version.split('.').map((v,i,a)=>a.slice(0,i+1).join('.')).join(',')
+const versions = version.split('.').map((v,i,a)=>a.slice(0,i+1).join('.')+suffix)
 
-console.log(versions)
+versions.join(versions[0]==='0'?'dev':'latest')
+
+console.log(versions.join(','))
