3f7d7dfc686e591aef9674ac9f677094414bd809
shynome <shynome@gmail.com>
2018-07-26 01:30:10 +0800

    11

--- set_tag.js
+++ set_tag.js
@@ -1,8 +1,18 @@
 
-const [ version, suffix='' ] = /**@type {string}*/(process.env.npm_package_version).split('-')
+/**
+ * 
+ * @param {string} npm_package_version 
+ */
+function get_tags(npm_package_version){
 
-const versions = version.split('.').map((v,i,a)=>a.slice(0,i+1).join('.')+suffix)
+  const [ version, suffix='' ] = npm_package_version.split('-')
+  
+  const versions = version.split('.').map((v,i,a)=>a.slice(0,i+1).join('.')+(suffix && '-')+suffix)
+  
+  versions.join(versions[0]==='0'?'dev':'latest')
 
-versions.join(versions[0]==='0'?'dev':'latest')
+  return versions.join(',')
+  
+}
 
-console.log(versions.join(','))
+console.log(get_tags(process.env.npm_package_version))
